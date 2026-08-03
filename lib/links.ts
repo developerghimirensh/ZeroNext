802e746e6551577f286ab328d6c302dfaed65070
developerghimirensh <developerghimirensh@gmail.com>
2026-08-03 20:43:08 +0545

    links

---
+++ lib/links.ts
@@ -0,0 +1,16 @@
+const links=[
+    {
+        href: "/",
+        label: "Dashboard"
+    },
+    {
+        href: "/portfolio",
+        label: "Portfolio"
+    },
+    {
+        href: "/stocks",
+        label: "Stocks"
+    }
+]
+
+export default links;
